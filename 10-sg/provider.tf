6f97219f370651b97b5432a0f6d45ec8a3934753
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-04-29 10:56:22 +0530

    test

---
+++ 10-sg/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "83s-remote-state-devlop"
+    key            = "expense-dev-eks-sg"
+    region         = "us-east-1"
+    dynamodb_table = "83s-remote-state-devlop"
+  }
+
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
