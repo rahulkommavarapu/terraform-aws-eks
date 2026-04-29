4daa706e59495e492f6d85eadea4d350cc7ffc65
Rahul kommavarapu <Rahulkommavarapu225@gmail.com>
2026-04-29 15:31:55 +0530

    test

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -1,18 +1,21 @@
 module "vpc" {
-    #source = "../terraform-aws-vpc"
-    source = "git::https://github.com/rahulkommavarapu/terraform-aws-vpc.git?ref=main"
-    project_name = var.project_name
-    environment = var.environment
-    vpc_cidr = var.vpc_cidr
-    common_tags = var.common_tags
-    vpc_tags = var.vpc_tags
-    public_subnet_cidrs = var.public_subnet_cidrs
-    private_subnet_cidrs = var.private_subnet_cidrs
-    database_subnet_cidrs = var.database_subnet_cidrs
-    is_peering_required = true
-}
 
-# this can be included in module
+  # source = "../terraform-aws-vpc-2"
+
+
+  source = "git::https://github.com/rahulkommavarapu/terraform-aws-vpc-2.git?ref=main"
+
+  project_name          = var.project_name
+  environment           = var.environment
+  vpc_cidr              = var.vpc_cidr
+  common_tags           = var.common_tags
+  vpc_tags              = var.vpc_tags
+  public_subnet_cidrs   = var.public_subnet_cidrs
+  private_subnet_cidrs  = var.private_subnet_cidrs
+  database_subnet_cidrs = var.database_subnet_cidrs
+  is_peering_required   = true
+}
+# Create the DB Subnet Group for the Database connection
 resource "aws_db_subnet_group" "expense" {
   name       = "${var.project_name}-${var.environment}"
   subnet_ids = module.vpc.database_subnet_ids
@@ -20,7 +23,8 @@ resource "aws_db_subnet_group" "expense" {
   tags = merge(
     var.common_tags,
     {
-        Name = "${var.project_name}-${var.environment}"
+      Name = " ${var.project_name}-${var.environment}"
     }
+
   )
 }
